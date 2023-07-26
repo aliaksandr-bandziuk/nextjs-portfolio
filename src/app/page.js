332e2e1efa58af6c7ddaec96c3f13d76115953a3
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-07-27 00:53:02 +0200

    feat: add ga

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,32 @@
 import { About } from '@/components/About'
 import { Benefits } from '@/components/Benefits'
 import { Contact } from '@/components/Contact'
-import { CookieConsent } from '@/components/CookieConsent'
 import { Hero } from '@/components/Hero'
 import { Portfolio } from '@/components/Portfolio'
 import { Technologies } from '@/components/Technologies'
 
+import Script from 'next/script'
+
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <About />
-      <Technologies />
-      <Portfolio />
-      <Benefits />
-      <Contact />
-      {/* <CookieConsent /> */}
-    </main>
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-FYN41J3DJK" />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FYN41J3DJK');
+        `}
+      </Script>
+      <main>
+        <Hero />
+        <About />
+        <Technologies />
+        <Portfolio />
+        <Benefits />
+        <Contact />
+      </main>
+    </>
   )
 }
