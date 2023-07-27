ea41a01f26c730d29c1cc63d017408497bf78668
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-07-27 23:29:17 +0200

    feat: add ga code

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Montserrat } from 'next/font/google'
 import TheHeader from '@/components/TheHeader/TheHeader'
 import TheFooter from '@/components/TheFooter/TheFooter'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -18,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics GA_MEASUREMENT_ID="G-MY8KLK2YZY" />
       <body className={montserrat.className}>
         <TheHeader />
         {children}
