ccad876bc5386402bebb6a89902a9f4d1f27c397
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-08-02 16:07:39 +0200

    old analytics

--- src/app/page.js
+++ src/app/page.js
@@ -10,14 +10,17 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z3712JXEP8" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-MY8KLK2YZY" />
       <Script id='google-analytics'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
+          gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'analytics_storage': 'denied',
+          });
           gtag('js', new Date());
-
-          gtag('config', 'G-Z3712JXEP8');
+          gtag('config', 'G-MY8KLK2YZY');
         `}
       </Script>
       <main>
