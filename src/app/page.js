7c00c0aa597995be23e65e817fc852af76a2dc82
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-08-03 19:02:47 +0200

    edit new tag

--- src/app/page.js
+++ src/app/page.js
@@ -11,14 +11,16 @@ export default function Home() {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-3S7ZLYWVWK"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DS0C09N5J1"
       />
       <Script id='google-analytics'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-3S7ZLYWVWK');
+
+          gtag('config', 'G-DS0C09N5J1');
         `}
       </Script>
       <main>
